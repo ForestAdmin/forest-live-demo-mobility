68d3e07fe7352abe3817d3481448dfa72f4a4514
steveb <bunlon.steve@gmail.com>
2022-11-22 16:48:27 +0100

    feat(live-demo): add live-demo endless token and protection

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const {
 
 const app = express();
 
+app.use((req, res, next) => {
+  req.headers.authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIzNjk1IiwiZW1haWwiOiJzdGV2ZWJAZm9yZXN0YWRtaW4uY29tIiwiZmlyc3ROYW1lIjoiU3RldmUiLCJsYXN0TmFtZSI6IkJ1bmxvbiIsInRlYW0iOiJPcGVyYXRpb25zICIsInJvbGUiOiJBZG1pbiIsInRhZ3MiOltdLCJyZW5kZXJpbmdJZCI6MTE0NTk3LCJpYXQiOjE2NDI3NjA1Mjl9.pEsxmJCCTMYHHQVGi70OkJFk7GdCQQTHqyN3VVtpkBs';
+  next();
+});
+
 let allowedOrigins = [/\.forestadmin\.com$/, /localhost:\d{4}$/];
 
 if (process.env.CORS_ORIGINS) {
@@ -50,6 +55,21 @@ app.use('/forest', (request, response, next) => {
   if (PUBLIC_ROUTES.includes(request.url)) {
     return next();
   }
+
+  if (request.method !== 'GET'
+      && !request.originalUrl.startsWith('/forest/stats')
+      && !request.originalUrl.includes('/hooks/load')
+      && !request.originalUrl.includes('/hooks/change')
+  ) {
+    const errorMessage = 'You can only read data on this public demo application.';
+
+    if (request.originalUrl.startsWith('/forest/actions/')) {
+      return response.status(400).send({ error: errorMessage });
+    }
+
+    return response.status(403).send(errorMessage);
+  }
+
   return ensureAuthenticated(request, response, next);
 });
 
